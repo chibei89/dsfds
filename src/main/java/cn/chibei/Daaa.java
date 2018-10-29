d189f80dd0fa94e2e4ba5af69c6e16191c3d27c9
chibei89 <chibei89@163.com>
2018-10-29 19:30:17 +0800

    sfdsdfdsf

---
+++ src/main/java/cn/chibei/Daaa.java
@@ -0,0 +1,11 @@
+package main.java.cn.chibei;
+
+public class Daaa {
+    public static void main(String[] args) {
+        System.out.println("广东人吃福建人");
+        System.out.println("广东人吃福建人");
+        System.out.println("广东人吃福建人");
+        System.out.println("广东人吃福建人");
+        System.out.println("广东人吃福建人");
+    }
+}
